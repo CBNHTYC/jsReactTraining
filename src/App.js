91128419fb52168af38878eda5bb6dce44a676da
Куприянов Павел <cbnhtyc@gmail.com>
2019-04-28 16:23:02 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import Info from "./components/Info";
+import Form from "./components/Form";
+import Detail from "./components/Detail";
+import DetailSimple from "./components/DetailSimple";
+
+const BASE_URI = "http://localhost:8080/fc/rest";
 
 class App extends React.Component {
-  render() {
-    return (
-        <div>
-          <Info/>
-        </div>
-    );
-  }
+
+    state = {
+        id: undefined,
+        name: undefined,
+        error: undefined
+    };
+
+    getDetails = async (event) => {
+        event.preventDefault();
+        const type = event.target.elements.type.value;
+
+        if (type) {
+            const apiUrl = await fetch(`${BASE_URI}/getDetails?category=${type}`);
+            const data = await apiUrl.json();
+            console.log(data);
+
+            const detailDtoList = data.detailDtoList.map(item => item);
+            console.log(detailDtoList);
+            const names = detailDtoList.map(item => item.name);
+            console.log(names);
+
+            this.setState({
+                id: detailDtoList[0].detailId,
+                name: detailDtoList[0].name,
+                error: undefined
+            });
+        } else {
+            this.setState({
+                id: undefined,
+                name: undefined,
+                error: "Введите тип запрашиваемых характеристик"
+            });
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Info/>
+                <Form getDetails={this.getDetails}/>
+                <DetailSimple
+                    id={this.state.id}
+                    name={this.state.name}
+                    error={this.state.error}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
